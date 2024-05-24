566bb72bdee3b374c3904b9134a11c7d947fdad8
JhoanSB0608 <156533638+JhoanSB0608@users.noreply.github.com>
2024-05-24 15:30:22 -0500

    feat: :zap: Se cambiaron las x-rapidapi-key

--- src/components/reproductor.js
+++ src/components/reproductor.js
@@ -155,7 +155,7 @@ class SearchSongs extends HTMLElement {
             const id = uri.split(':')[2];
             const typeOf = uri.split(':')[1];
             this.shadowRoot.innerHTML = `
-                <iframe class="spotify-iframe" width="100%" height="400" src="https://open.spotify.com/embed/${typeOf}/${id}" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/0MkMu019XCFwwuL1ftHKmQ?utm_source=generator" width="100%" height="600" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
             `;
         } else {
             this.shadowRoot.innerHTML = '';
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 export default defineConfig({
         server: {
                 host:"172.16.101.146",
-                port:5691
+                port:5697
         }
 })
